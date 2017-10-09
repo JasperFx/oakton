f3c9333b804e2b9c6c7eb6495e971c779ed392aa
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-10-09 15:30:23 -0500

    docs on bootstrapping

--- src/OaktonSample/Program.cs
+++ src/OaktonSample/Program.cs
@@ -1,22 +1,83 @@
 ﻿using System;
 using System.Reflection;
 using Oakton;
+using StructureMap;
 
 namespace OaktonSample
 {
     public class Program
     {
-        public static void Main(string[] args)
+        // SAMPLE: bootstrapping-command-executor
+        public static int Main(string[] args)
         {
             var executor = CommandExecutor.For(_ =>
             {
+                // Automatically discover and register
+                // all OaktonCommand's in this assembly
                 _.RegisterCommands(typeof(Program).GetTypeInfo().Assembly);
+                
+                // You can also add commands explicitly from
+                // any assembly
+                _.RegisterCommand<HelloCommand>();
+                
+                // In the absence of a recognized command name,
+                // this is the default command to try to 
+                // fit to the arguments provided
                 _.DefaultCommand = typeof(ColorCommand);
+
+                
+                _.ConfigureRun = run =>
+                {
+                    // you can use this to alter the values
+                    // of the inputs or actual command objects
+                    // just before the command is executed
+                };
+                
+                // This is strictly for the as yet undocumented
+                // feature in stdocs to generate and embed usage information
+                // about console tools built with Oakton into
+                // stdocs generated documentation websites
+                _.SetAppName("MyApp");
             });
 
-            executor.Execute(args);
+            // See the page on Opts files
+            executor.OptionsFile = "myapp.opts";
+
+            return executor.Execute(args);
+        }
+        // ENDSAMPLE
+        
+        
+        // SAMPLE: bootstrapping-with-custom-command-factory
+        public static void Bootstrapping(IContainer container)
+        {
+            var executor = CommandExecutor.For(_ =>
+            {
+                // do the other configuration of the CommandFactory
+            }, new StructureMapCommandCreator(container));
+        }
+        // ENDSAMPLE
+    }
+    
+
+    // SAMPLE: StructureMapCommandCreator
+    public class StructureMapCommandCreator : ICommandCreator
+    {
+        private readonly IContainer _container;
+
+        public StructureMapCommandCreator(IContainer container)
+        {
+            _container = container;
+        }
+
+        public IOaktonCommand Create(Type commandType)
+        {
+            return (IOaktonCommand)_container.GetInstance(commandType);
         }
     }
+    // ENDSAMPLE
+
+
 
     public class ColorInput
     {
