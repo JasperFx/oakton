1d936bd403ee2c78742c22db6a04e1c8ae7b3c3d
Jeremy D. Miller <jeremydmiller@yahoo.com>
2017-10-10 08:54:19 -0500

    got the Mac friendly build.sh doing what it's supposed to do

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 dotnet restore src/Oakton.Testing/Oakton.Testing.csproj
-dotnet test src/Oakton.Testing/Oakton.Testing.csproj
+dotnet test src/Oakton.Testing/Oakton.Testing.csproj --framework netcoreapp1.1
