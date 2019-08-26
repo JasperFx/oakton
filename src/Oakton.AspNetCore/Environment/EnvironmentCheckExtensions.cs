fd301f745215cba7a511e5a3831233bce4ff7f0a
jeremydmiller <jeremydmiller@yahoo.com>
2019-08-26 13:17:15 -0500

    tests on LambdaCheck, renamed nomenclature to "environment checks"

--- src/Oakton.AspNetCore/Environment/EnvironmentCheckExtensions.cs
+++ src/Oakton.AspNetCore/Environment/EnvironmentCheckExtensions.cs
@@ -7,7 +7,7 @@ namespace Oakton.AspNetCore.Environment
 {
     public static class EnvironmentCheckExtensions
     {
-        public static void EnvironmentTest(this IServiceCollection services,
+        public static void EnvironmentCheck(this IServiceCollection services,
             string description,
             Func<IServiceProvider, CancellationToken, Task> test)
         {
@@ -15,36 +15,36 @@ namespace Oakton.AspNetCore.Environment
             services.AddSingleton<IEnvironmentCheck>(check);
         }
 
-        public static void EnvironmentTest(this IServiceCollection services, string description, Action action)
+        public static void EnvironmentCheck(this IServiceCollection services, string description, Action action)
         {
-            services.EnvironmentTest(description, (s, c) =>
+            services.EnvironmentCheck(description, (s, c) =>
             {
                 action();
                 return Task.CompletedTask;
             });
         }
         
-        public static void EnvironmentTest<T>(this IServiceCollection services, string description, Action<T> action)
+        public static void EnvironmentCheck<T>(this IServiceCollection services, string description, Action<T> action)
         {
-            services.EnvironmentTest(description, (s, c) =>
+            services.EnvironmentCheck(description, (s, c) =>
             {
                 action(s.GetService<T>());
                 return Task.CompletedTask;
             });
         }
         
-        public static void EnvironmentTest<T1, T2>(this IServiceCollection services, string description, Action<T1, T2> action)
+        public static void EnvironmentCheck<T1, T2>(this IServiceCollection services, string description, Action<T1, T2> action)
         {
-            services.EnvironmentTest(description, (s, c) =>
+            services.EnvironmentCheck(description, (s, c) =>
             {
                 action(s.GetService<T1>(), s.GetService<T2>());
                 return Task.CompletedTask;
             });
         }
         
-        public static void EnvironmentTest<T1, T2, T3>(this IServiceCollection services, string description, Action<T1, T2, T3> action)
+        public static void EnvironmentCheck<T1, T2, T3>(this IServiceCollection services, string description, Action<T1, T2, T3> action)
         {
-            services.EnvironmentTest(description, (s, c) =>
+            services.EnvironmentCheck(description, (s, c) =>
             {
                 action(s.GetService<T1>(), s.GetService<T2>(), s.GetService<T3>());
                 return Task.CompletedTask;
@@ -52,27 +52,27 @@ namespace Oakton.AspNetCore.Environment
         }
         
         
-        public static void EnvironmentTest<T>(this IServiceCollection services, string description, Func<T, CancellationToken, Task> action)
+        public static void EnvironmentCheck<T>(this IServiceCollection services, string description, Func<T, CancellationToken, Task> action)
         {
-            services.EnvironmentTest(description, (s, c) =>
+            services.EnvironmentCheck(description, (s, c) =>
             {
                 action(s.GetService<T>(), c);
                 return Task.CompletedTask;
             });
         }
         
-        public static void EnvironmentTest<T1, T2>(this IServiceCollection services, string description, Func<T1, T2, CancellationToken, Task> action)
+        public static void EnvironmentCheck<T1, T2>(this IServiceCollection services, string description, Func<T1, T2, CancellationToken, Task> action)
         {
-            services.EnvironmentTest(description, (s, c) =>
+            services.EnvironmentCheck(description, (s, c) =>
             {
                 action(s.GetService<T1>(), s.GetService<T2>(), c);
                 return Task.CompletedTask;
             });
         }
         
-        public static void EnvironmentTest<T1, T2, T3>(this IServiceCollection services, string description, Func<T1, T2, T3, CancellationToken, Task> action)
+        public static void EnvironmentCheck<T1, T2, T3>(this IServiceCollection services, string description, Func<T1, T2, T3, CancellationToken, Task> action)
         {
-            services.EnvironmentTest(description, (s, c) =>
+            services.EnvironmentCheck(description, (s, c) =>
             {
                 action(s.GetService<T1>(), s.GetService<T2>(), s.GetService<T3>(), c);
                 return Task.CompletedTask;
