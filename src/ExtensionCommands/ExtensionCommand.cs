4a11c3920fd0df71fc01723dabbe7a6533a0b213
jeremydmiller <jeremydmiller@yahoo.com>
2019-08-26 09:14:10 -0500

    Missed extension commands

---
+++ src/ExtensionCommands/ExtensionCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Oakton;
+
+[assembly:OaktonCommandAssembly]
+
+namespace ExtensionCommands
+{
+    public class ExtensionInput
+    {
+        
+    }
+    
+    [Description("An extension command loaded from another assembly")]
+    public class ExtensionCommand : OaktonCommand<ExtensionInput>
+    {
+        public override bool Execute(ExtensionInput input)
+        {
+            Console.WriteLine("I'm an extension command");
+            return true;
+        }
+    }
+}
