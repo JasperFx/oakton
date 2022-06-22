4d33f716a0a78e59a62758f020cc4de7f1b1bab7
Jeremy D. Miller <Jeremy.Miller@medeanalytics.com>
2022-06-22 12:30:21 -0500

    Documentation Update for 4.0.0

---
+++ assets/guide_host_extensions.md.ebab13dd.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,a as t}from"./app.012e36bd.js";const h='{"title":"Writing Extension Commands","description":"","frontmatter":{},"relativePath":"guide/host/extensions.md","lastUpdated":1655919011680}',o={},e=t("",11),p=[e];function c(i,l,u,r,d,m){return a(),n("div",null,p)}var b=s(o,[["render",c]]);export{h as __pageData,b as default};
---
+++ assets/guide_opts.md.be6d5075.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as e,a as n}from"./app.012e36bd.js";const h='{"title":"\\"Opts\\" Files","description":"","frontmatter":{},"relativePath":"guide/opts.md","lastUpdated":1655919011663}',t={},o=n("",16),p=[o];function c(i,l,r,u,d,m){return e(),a("div",null,p)}var f=s(t,[["render",c]]);export{h as __pageData,f as default};
