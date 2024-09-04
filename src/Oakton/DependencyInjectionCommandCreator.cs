3cc8cd1708243f6efedaf1a562dd96e0c6503438
Jeremy D. Miller <jeremydmiller@yahoo.com>
2024-09-04 15:36:54 -0500

    Quick change to di injector to make is dispose the scope

--- src/Oakton/DependencyInjectionCommandCreator.cs
+++ src/Oakton/DependencyInjectionCommandCreator.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
+using System.Threading.Tasks;
+using JasperFx.Core;
+using JasperFx.Core.Reflection;
+using Oakton.Help;
 
 namespace Oakton;
 
@@ -14,6 +19,7 @@ internal class DependencyInjectionCommandCreator : ICommandCreator
     public IOaktonCommand CreateCommand(Type commandType)
     {
         return ActivatorUtilities.CreateInstance(_serviceProvider, commandType) as IOaktonCommand;
+        //return new WrappedOaktonCommand(_serviceProvider, commandType);
     }
 
     public object CreateModel(Type modelType)
@@ -21,3 +27,31 @@ internal class DependencyInjectionCommandCreator : ICommandCreator
         return Activator.CreateInstance(modelType)!;
     }
 }
+
+internal class WrappedOaktonCommand : IOaktonCommand
+{
+    private readonly IServiceScope _scope;
+    private readonly IOaktonCommand _inner;
+
+    public WrappedOaktonCommand(IServiceProvider provider, Type commandType)
+    {
+        _scope = provider.CreateScope();
+        _inner = (IOaktonCommand)_scope.ServiceProvider.GetRequiredService(commandType);
+    }
+
+    public Type InputType => _inner.InputType;
+    public UsageGraph Usages => _inner.Usages;
+    public async Task<bool> Execute(object input)
+    {
+        try
+        {
+            return await _inner.Execute(input);
+        }
+        finally
+        {
+            _scope.SafeDispose();
+        }
+    }
+}
+
+
